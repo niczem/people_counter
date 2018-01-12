4be1abf68c03924369ec4058a880e8dc85678f67
niczem <nic@transparency-everywhere.com>
2018-01-12 17:45:40 +0100

    minor changes

---
+++ example_config.php
@@ -0,0 +1,31 @@
+<?php
+/*
+This file is published by transparency-everywhere with the best deeds.
+Check transparency-everywhere.com for further information.
+Licensed under the CC License, Version 4.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://creativecommons.org/licenses/by/4.0/legalcode
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+@author nicZem for tranpanrency-everywhere.com
+*/
+$host = 'localhost';// MySQL Hostname
+$database = 'people_counter';// MySQL Database
+$db_user = 'user';// MySQL Username
+$db_pass = 'password';// MySQL Password
+$db_port = '';// Database port - could be empty - default 3306;
+
+define('dbHost', $host);
+define('db', $database);
+define('dbUser', $db_user);
+define('dbPass', $db_pass);
+define('dbPort', $db_port);
+?>
